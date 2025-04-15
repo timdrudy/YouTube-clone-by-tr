27b2604a8971afcf01e43e7dc3a3253966d26628
Tim <timdrudy@gmail.com>
2025-04-15 11:28:42 -0500

    made 45 different recommended videos on each page, and the comments and info about each are appropriate when clicked

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -9,8 +9,11 @@ import jack from "../../assets/jack.png";
 import user_profile from "../../assets/user_profile.jpg";
 import { API_KEY, value_converter } from "../../data";
 import moment from "moment";
+import { useParams } from "react-router-dom";
 
-const PlayVideo = ({videoId}) => {
+const PlayVideo = () => {
+
+const {videoId} = useParams();
 
 const [apiData, setApiData] = useState(null);
 const [channelData, setChannelData] = useState(null);
@@ -34,7 +37,7 @@ const fetchOtherData = async () => {
 
 useEffect(()=> {
     fetchVideoData();
-},[])
+},[videoId])
 
 useEffect(()=>{
     fetchOtherData();
